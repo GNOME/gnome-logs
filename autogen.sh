13e69df7a5f2a353fd4c8b90620ab4ea19f55896
David King <davidk@gnome.org>
2015-01-19 22:01:28 +0000

    Removed deprecated variables from autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -4,11 +4,9 @@
 srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 
-PKG_NAME="gnome-logs"
-
 (test -f $srcdir/src/gl-main.c) || {
     echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
-    echo " top-level $PKG_NAME directory"
+    echo " top-level gnome-logs directory"
     exit 1
 }
 
@@ -19,4 +17,4 @@ which gnome-autogen.sh || {
     exit 1
 }
 
-USE_GNOME2_MACROS=1 . gnome-autogen.sh
+. gnome-autogen.sh
