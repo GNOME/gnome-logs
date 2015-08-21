f5fe6f36966051108b10ada826cfc6cd0db47bff
David King <davidk@gnome.org>
2015-08-21 12:36:50 +0100

    Add config.h include where gettext is used

--- src/gl-eventtoolbar.c
+++ src/gl-eventtoolbar.c
@@ -16,6 +16,7 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include "config.h"
 #include "gl-eventtoolbar.h"
 
 #include <glib/gi18n.h>
--- src/gl-util.c
+++ src/gl-util.c
@@ -17,6 +17,7 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include "config.h"
 #include "gl-util.h"
 
 #include <glib/gi18n.h>
