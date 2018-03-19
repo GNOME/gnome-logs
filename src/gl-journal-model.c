dadd7d545a58e4812b454c70a52daf4c9fd3e0ab
Jonathan Kang <jonathankang@gnome.org>
2018-03-19 15:35:56 +0800

    free GlQuery when journal model is disposing
    GlJournalModel contains GlQuery in its private structure. And GlQuery
    should be free using gl_query_free() to avoid memory leak.
    
    (cherry picked from commit 93fcf1af6fe28a4501d273f562068dddedca575e)

--- src/gl-journal-model.c
+++ src/gl-journal-model.c
@@ -66,6 +66,7 @@ static gboolean gl_query_check_journal_end (GlQuery *query, GlJournalEntry *entr
 static gboolean gl_row_entry_check_message_similarity (GlRowEntry *current_row_entry,
                                                        GlRowEntry *prev_row_entry);
 static void gl_journal_model_add_header (GlJournalModel *model);
+static void gl_query_free (GlQuery *query);
 
 
 G_DEFINE_TYPE (GlRowEntry, gl_row_entry, G_TYPE_OBJECT);
@@ -238,6 +239,11 @@ gl_journal_model_dispose (GObject *object)
     GlJournalModel *model = GL_JOURNAL_MODEL (object);
 
     gl_journal_model_stop_idle (model);
+
+    if (model->query)
+    {
+        gl_query_free (model->query);
+    }
 
     if (model->entries)
     {
